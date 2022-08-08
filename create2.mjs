7707ef29be8c3132068598527e83846d2fc9782b
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2022-08-08 17:43:46 +0800

    add create2.mjs

---
+++ create2.mjs
@@ -0,0 +1,90 @@
+var asm = (c) => {
+    var bin = {};
+    `CALLVALUE 34 CALLDATASIZE 36 CALLDATACOPY 37
+     MSTORE 52 JUMPI 57 JUMPDEST 5B
+     PUSH1 60 PUSH2 61 PUSH4 63 LOG0 A0
+     CREATE2 F5 RETURN F3 SELFDESTRUCT FF`
+        .replace(/(\w+)\s*(\w+)\s*/g, (_,k,v) => bin[k] = v); // make bin
+
+    return c.replace(/\/\/.*?\n/g, "")                // *no comment*
+        .replace(/\s*(\w+)\s*/g, (_,s) => bin[s]||s); // replace with bin
+}
+
+// no value => bomb  (0x3DFF)
+// has value => cafe (0xCafeBabe)
+var bombOrCafe = asm(`
+CALLVALUE
+PUSH1 0F  // jump to 0x0F if value != 0
+JUMPI  
+
+PUSH2 3DFF
+PUSH1 00
+MSTORE    // mem[1E...1E+02) = 0x3DFF
+PUSH1 02
+PUSH1 1E
+RETURN
+
+JUMPDEST  // PC == 0x0F here
+PUSH4 CAFEBABE
+PUSH1 00
+MSTORE    // mem[1C...1C+04) = 0xCAFEBABE
+PUSH1 04
+PUSH1 1C
+RETURN
+`);
+console.log("bombOrCafe: " + bombOrCafe);
+
+var calldataAsInitFactory = asm(`
+CALLDATASIZE  // CALLDATACOPY arg 2: size
+PUSH1 00      // CALLDATACOPY arg 1: call data src offset
+PUSH1 00      // CALLDATACOPY arg 0: memory dest offset
+CALLDATACOPY  // mem[0...CALLDATASIZE) = call data
+
+PUSH1 00      // CREATE2 arg 3: salt
+CALLDATASIZE  // CREATE2 arg 2: init code size
+PUSH1 00      // CREATE2 arg 1: memory src offset
+CALLVALUE     // CREATE2 arg 0: endowment(value): pass through
+CREATE2       // send the init code mem[0 ... init code size)
+
+              // optional: log the result contract addr
+              // which is on the top of the stack
+PUSH1 00
+MSTORE        // mem[0...32) = CREATE2 result addr
+PUSH1 14      // 1: size 0x14 = 20 bytes (160 bits)
+PUSH1 0C      // 0: offset 0x0C = 12 bytes
+LOG0          // LOG the result addr
+`);
+
+// append any body after this fixed, 0x0B-byte deployer
+var deployer = "600B380380600B3D393DF3";
+
+
+var ethers = await import("ethers");
+var provider = new ethers.providers.AlchemyProvider(
+    process.env.NETWORK, process.env.ALCHEMY_API_KEY);
+var signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+var txReceipt = async (t) => (await signer.sendTransaction(t)).wait();
+
+// STEP 0: deploy the factory contract
+var factoryAddr = (await txReceipt(
+    {data: "0x" + deployer + calldataAsInitFactory})).contractAddress;
+console.log("factoryAddr: " + factoryAddr);
+
+
+// STEP 1: value 0 => depoly "bomb"
+var receipt1 = await txReceipt(
+    {data: "0x" + bombOrCafe, to: factoryAddr, value: 0});
+console.log("addr1: " + receipt1.logs[0].data
+    + " body1: " + await provider.getCode(receipt1.logs[0].data));
+
+
+// STEP 2: trigger the bomb
+await txReceipt({to: receipt1.logs[0].data});
+
+
+// STEP 3: value 1 => re-deploy "cafe"
+var receipt2 = await txReceipt(
+    {data: "0x" + bombOrCafe, to: factoryAddr, value: 1});
+console.log("addr2: " + receipt2.logs[0].data
+    + " body2: " + await provider.getCode(receipt2.logs[0].data));
